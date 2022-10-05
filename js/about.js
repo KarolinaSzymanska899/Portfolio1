e1987aac960d49d3b9320572f92e30986547baa6
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-10-06 00:14:56 +0200

    making a slideout menu

---
+++ js/about.js
@@ -0,0 +1,6 @@
+const arrowsDown = document.querySelectorAll("#arrow-down");
+const extraSentences = document.querySelectorAll("#extra-sentence");
+
+arrowsDown.onclick = function () {
+  extraSentences[i].style.display = "block";
+};
