3fb0aa62546bd9b3c7af0e9e51d0682882b9c687
Karolina Szymanska <karolinaszymanska899@gmail.com>
2022-10-06 13:37:06 +0200

    reorganising html content, styling, removing some js files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// MOBILE NAVIGATION
+
+let isMenuOpen = false;
+
+const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+
+menuButton.addEventListener("click", () => {
+  isMenuOpen = !isMenuOpen;
+
+  if (isMenuOpen) {
+    menuButton.classList.add("open");
+    menu.classList.add("menu-open");
+  } else {
+    menuButton.classList.remove("open");
+    menu.classList.remove("menu-open");
+  }
+});
